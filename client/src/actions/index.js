06aff710d8d30145cf53fbdde4f67756af42294d
NachoPasser <nachopasser@gmail.com>
2022-08-02 12:33:01 -0300

    Otro fix más

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,22 +1,22 @@
 import axios from 'axios'
-
+import { API_URL } from '../config/enviroment'
 export const getDogs = () => {
     return function(dispatch){
-        axios.get('http://localhost:3001/dogs')
+        axios.get(`${API_URL}/dogs`)
         .then(dogs => dispatch({type: 'GET_DOGS', payload: dogs.data}))
     }
 }
 
 export const getTemperaments = () => {
     return function(dispatch){
-        axios.get('http://localhost:3001/temperaments')
+        axios.get(`${API_URL}/temperaments`)
         .then(tempers => dispatch({type: 'GET_TEMPERAMENTS', payload:tempers.data}))
     }
 }
 
 export const getDogsByName = (name) => {
     return function(dispatch){
-        axios.get(`http://localhost:3001/dogs?name=${name}`)
+        axios.get(`${API_URL}/dogs?name=${name}`)
         .then(dogs => {
             dispatch({type: 'DOG_NOT_FOUND', payload: ''})
             dispatch({type: 'GET_DOGS_BY_NAME', payload: dogs.data})})
@@ -28,7 +28,7 @@ export const getDogsByName = (name) => {
 
 export const getDogById = (id) => {
     return function(dispatch){
-        axios.get(`http://localhost:3001/dogs/${id}`)
+        axios.get(`${API_URL}/dogs/${id}`)
         .then(dog => dispatch({type: 'GET_DOG_BY_ID', payload: dog.data}))
         .catch(e => dispatch({type: 'GET_DOG_BY_ID', payload: {}}))
     }
